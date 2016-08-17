192337b1d7242bd5d4e0adff9eb0b0e381a86d3c
Barnabas Chan <barny@post.com>
2016-08-17 11:48:58 +0800

    remove enabling of crossorogin from express.js

--- config/express.js
+++ config/express.js
@@ -16,12 +16,6 @@ module.exports = function() {
     app.use(compress());
   }
 
-  app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "OriginX-Requested-With, Content-Type, Accept");
-    next();
-  });
-
   app.use(bodyParser.urlencoded({
     extended: true
   }));
